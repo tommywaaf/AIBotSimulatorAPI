63d08f60739752c3669ab7bd2ecd119b44aa1076
tommy michaud <tommy01337@gmail.com>
2023-02-16 23:07:57 -0500

    test

--- AIBotSimulatorAPI/AIBotSimulatorAPI.py
+++ AIBotSimulatorAPI/AIBotSimulatorAPI.py
@@ -61,7 +61,7 @@ def get_bot_image(bot_id):
     image_data = BytesIO(image["data"])
 
     # Return the image as a file
-    return send_file(image_data, mimetype=image["mimetype"])
+    return send_file(BytesIO(image_data), mimetype=image["mimetype"])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
