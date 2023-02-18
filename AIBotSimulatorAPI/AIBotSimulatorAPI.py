41bff00f19cdf6b9f6cf551fe9bf36b3bf63ac03
tommy michaud <tommy01337@gmail.com>
2023-02-18 01:28:23 -0500

    f

--- AIBotSimulatorAPI/AIBotSimulatorAPI.py
+++ AIBotSimulatorAPI/AIBotSimulatorAPI.py
@@ -218,8 +218,7 @@ def post_generate_battle():
         losing_bot = team2
     else:
         losing_bot = team1
-    winner = int(winner)
-
+    winner = str(new_response["winner"])
     db.bots.update_one({'botId': winner}, {'$inc': {'wins': 1}})
     db.bots.update_one({'botId': losing_bot['botId']}, {'$inc': {'losses': 1}})
 
